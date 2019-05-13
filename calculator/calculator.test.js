6fa3e96ffec6898f7da62db9a7837e48a7770492
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-13 13:17:20 -0400

    Adds a second test for same method

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -6,7 +6,11 @@ describe('calculator.js', () => {
     it('should return the sum of two numbers', () => {
       expect(calculator.add(2,2)).toBe(4);
       expect(calculator.add(2,1)).toBe(3);
-      expect(calculator.add(2,0)).toBe(2);
+      expect(calculator.add(0,1)).toBe(1);
+    })
+
+    it('should return 0 when called without args', () => {
+      expect(calculator.add()).toBe(0);
     })
   })
 })
